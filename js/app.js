c08302d7f903b33ee2d9b684063b99d25d09976b
Kyle King <bkkcreative16@gmail.com>
2021-01-19 16:52:12 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,138 @@
+const ul = document.querySelector(".choices");
+const list = document.querySelectorAll(".choices__choice");
+const computerChoiceDOM = document.querySelector(".computer-choice");
+const btn = document.querySelector(".btn");
+const answers = document.querySelector(".answers");
+const results = document.querySelector(".results");
+const result = document.querySelector(".result");
+const scoreDOM = document.querySelector(".score");
+const resetBtn = document.querySelector(".reset-btn");
+const playerChoiceDOM = document.querySelector(".player-choice");
+let playerChoice;
+const choices = ["paper", "scissors", "rock"];
+let score = Number(localStorage.getItem("score"));
+let didWin;
+scoreDOM.textContent = score;
+
+list.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    computerChoiceDOM.classList.remove("answers__computer");
+    computerChoiceDOM.classList.add("waiting-circle");
+
+    ul.style.display = "none";
+    answers.style.display = "flex";
+    setTimeout(function () {
+      computerChoiceDOM.style.display = "flex";
+      computerChoiceDOM.classList.remove("waiting-circle");
+      computerChoiceDOM.classList.add("answers__computer");
+      const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+      createImage(computerChoiceDOM, randomChoice);
+    }, 3000);
+    setTimeout(function () {
+      results.style.display = "flex";
+      if (didWin === "tie") {
+        result.textContent = "It's a Draw";
+      } else if (!didWin) {
+        result.textContent = "you lost";
+        if (scoreDOM.textContent < 1) {
+          scoreDOM.textContent = 0;
+        } else {
+          scoreDOM.textContent = score - 1;
+        }
+
+        localStorage.setItem("score", scoreDOM.textContent);
+      } else if (didWin) {
+        result.textContent = "you won";
+        scoreDOM.textContent = score + 1;
+        localStorage.setItem("score", scoreDOM.textContent);
+      }
+    }, 4000);
+
+    setTimeout(function () {
+      if (
+        playerChoiceDOM.classList.contains("paper") &&
+        computerChoiceDOM.classList.contains("paper")
+      ) {
+        didWin = "tie";
+      }
+      if (
+        playerChoiceDOM.classList.contains("rock") &&
+        computerChoiceDOM.classList.contains("rock")
+      ) {
+        didWin = "tie";
+      }
+      if (
+        playerChoiceDOM.classList.contains("scissors") &&
+        computerChoiceDOM.classList.contains("scissors")
+      ) {
+        didWin = "tie";
+      }
+      if (
+        playerChoiceDOM.classList.contains("scissors") &&
+        computerChoiceDOM.classList.contains("paper")
+      ) {
+        didWin = true;
+      }
+      if (
+        playerChoiceDOM.classList.contains("scissors") &&
+        computerChoiceDOM.classList.contains("rock")
+      ) {
+        didWin = false;
+      }
+      if (
+        playerChoiceDOM.classList.contains("paper") &&
+        computerChoiceDOM.classList.contains("rock")
+      ) {
+        didWin = true;
+      }
+      if (
+        playerChoiceDOM.classList.contains("paper") &&
+        computerChoiceDOM.classList.contains("scissors")
+      ) {
+        didWin = false;
+      }
+      if (
+        playerChoiceDOM.classList.contains("rock") &&
+        computerChoiceDOM.classList.contains("scissors")
+      ) {
+        didWin = true;
+      }
+      if (
+        playerChoiceDOM.classList.contains("rock") &&
+        computerChoiceDOM.classList.contains("paper")
+      ) {
+        didWin = false;
+      }
+    }, 3000);
+    if (choice.classList.contains("rock")) {
+      createImage(playerChoiceDOM, "rock");
+    } else if (choice.classList.contains("scissors")) {
+      createImage(playerChoiceDOM, "scissors");
+    } else if (choice.classList.contains("paper")) {
+      createImage(playerChoiceDOM, "paper");
+    }
+  });
+});
+
+btn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+const createImage = (who, choice) => {
+  who.innerHTML = `
+  <img src="./images/icon-${choice}.svg" alt="${choice}" />
+  `;
+  if (choice === "paper") {
+    who.classList.add("paper");
+  } else if (choice === "rock") {
+    who.classList.add("rock");
+  } else if (choice === "scissors") {
+    who.classList.add("scissors");
+  }
+};
+
+resetBtn.addEventListener("click", () => {
+  localStorage.clear();
+  scoreDOM.textContent = score;
+  window.location.reload();
+});
