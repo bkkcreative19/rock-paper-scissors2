0eeb60a56cffb8dc07be1286a600a95a3e623695
Kyle King <bkkcreative16@gmail.com>
2021-01-19 17:25:07 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -14,6 +14,32 @@ let score = Number(localStorage.getItem("score"));
 let didWin;
 scoreDOM.textContent = score;
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btnn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btnn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 list.forEach((choice) => {
   choice.addEventListener("click", (e) => {
     computerChoiceDOM.classList.remove("answers__computer");
