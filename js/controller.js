bd39941344c72652d687a504e89b2c1e561402dc
Kyle King <bkkcreative16@gmail.com>
2021-01-22 14:38:15 -0600

    fixed bug

--- js/controller.js
+++ js/controller.js
@@ -1,44 +1,19 @@
 const ul = document.querySelector(".choices");
 const list = document.querySelectorAll(".choices__choice");
-const computerChoiceDOM = document.querySelector(".computer-choice");
+const computerChoiceDOM = document.querySelector("#computer-choice");
+const playerChoiceDOM = document.querySelector("#player-choice");
 const btn = document.querySelector(".btn");
-const answers = document.querySelector(".answers");
-const results = document.querySelector(".results");
-const result = document.querySelector(".result");
-const scoreDOM = document.querySelector(".score");
-const resetBtn = document.querySelector(".reset-btn");
-const playerChoiceDOM = document.querySelector(".player-choice");
+const answers = document.querySelector("#answers");
+const results = document.querySelector("#results");
+const result = document.querySelector("#result");
+const scoreDOM = document.querySelector("#score");
+const resetBtn = document.querySelector("#reset-btn");
+
 let playerChoice;
 const choices = ["paper", "scissors", "rock"];
 let score = Number(localStorage.getItem("score"));
 let didWin;
 scoreDOM.textContent = score;
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btnn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btnn.onclick = function () {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 
 list.forEach((choice) => {
   choice.addEventListener("click", (e) => {
@@ -162,3 +137,29 @@ resetBtn.addEventListener("click", () => {
   scoreDOM.textContent = score;
   window.location.reload();
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btnn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btnn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
